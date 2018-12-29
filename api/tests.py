345985e1f5fa64eb2f29ea88f099497948d941f6
Jonathan Tredway <jptredway@gmail.com>
2018-12-29 13:19:25 -0600

    Add unit test for Photo model

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Photo
 
 # Create your tests here.
+class PhotoTestCase(TestCase):
+  def setUp(self):
+    Photo.objects.create(name="cat pic", src="kitty.jpg")
+    Photo.objects.create(name="cat pic 2.0", src="bigkitty.jpg")
+  
+  def test_assert_photos_are_created(self):
+    first = Photo.objects.get(name="cat pic")
+    second = Photo.objects.get(name="cat pic 2.0")
+    self.assertEqual(first.src, "kitty.jpg")
+    self.assertEqual(second.src, "bigkitty.jpg")
